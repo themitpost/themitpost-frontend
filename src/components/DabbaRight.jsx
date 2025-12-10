6d5bea7eba0c8a70f6106064c3c82da25b3249dc
aditirishiraj3008 <aditi1.mitmpl2023@learner.manipal.edu>
2025-12-10 22:00:25 +0530

    Added social media links

--- src/components/DabbaRight.jsx
+++ src/components/DabbaRight.jsx
@@ -3,29 +3,50 @@ import React from "react";
 import "./Dabba.css";
 
 const DabbaRight = () => {
-	return (
-		<nav className="dabba dabba-right">
-			<section className="dabba-content">
-				<h2>About</h2>
-				<ul>
-					<li className="category">About Us</li>
-					<li className="category">The Board of 2025-26</li>
-					<li className="category">Instagram</li>
-					<li className="category">LinkedIn</li>
-					<li className="category">From the Editor's Desk</li>
-				</ul>
-			</section>
-			<section className="dabba-content">
-				<h2>Resources</h2>
-				<ul>
-					<li className="category">MIT Information Brochure</li>
-					<li className="category">Academic Calendar</li>
-					<li className="category">All Branches Syllabus</li>
-					<li className="category">CGPA Conversion Factor</li>
-				</ul>
-			</section>
-		</nav>
-	);
+  return (
+    <nav className="dabba dabba-right">
+      <section className="dabba-content">
+        <h2>About</h2>
+        <ul>
+          <li className="category">About Us</li>
+          <li className="category">The Board of 2025-26</li>
+          <li className="category">From the Editor's Desk</li>
+        </ul>
+      </section>
+      <section className="social-media">
+        <a
+          class="icon instagram"
+          target="_blank"
+          href="https://www.instagram.com/mit.post/?next=%2F&hl=en"
+        ></a>
+        <a
+          class="icon linkedin"
+          target="_blank"
+          href="https://www.linkedin.com/company/themitpost/"
+        ></a>
+        <a
+          class="icon spotify"
+          target="_blank"
+          href="https://open.spotify.com/user/lu00lx0xzwbjohh4ye9h4nfsz?si=1c6dc1763fa94dfa"
+        ></a>
+        <a
+          class="icon facebook"
+          target="_blank"
+          href="https://www.facebook.com/themitpost/"
+        ></a>
+      </section>
+
+      <section className="dabba-content">
+        <h2>Resources</h2>
+        <ul>
+          <li className="category">MIT Information Brochure</li>
+          <li className="category">Academic Calendar</li>
+          <li className="category">All Branches Syllabus</li>
+          <li className="category">CGPA Conversion Factor</li>
+        </ul>
+      </section>
+    </nav>
+  );
 };
 
 export default DabbaRight;
